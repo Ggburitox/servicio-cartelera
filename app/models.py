f902df6dd65c395b614b4de9e23723b31fe29ee6
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 18:11:54 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,5 +6,5 @@ class Pelicula(Base):
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String(100), nullable=False)
     genero = Column(String(50))
-    duracion = Column(Integer)  # Minutos
+    duracion = Column(Integer)  # en minutos
     fecha_estreno = Column(DateTime)
