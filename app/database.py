811b23cca31eb7818595fa50a08ffc5a21fb1ab2
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 21:58:14 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,20 @@
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-dotenv_path = Path('.') / '.env'
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST")
-MYSQL_PORT = os.getenv("MYSQL_PORT")
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_HOST = os.getenv("MYSQL_HOST")  # IP privada MV BD
+MYSQL_PORT = os.getenv("MYSQL_PORT")  # 8008
+MYSQL_USER = os.getenv("MYSQL_USER")  # root
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")  # utec
+MYSQL_DB = os.getenv("MYSQL_DB")  # bd_cartelera
 
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
-)
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
