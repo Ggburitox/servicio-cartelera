a0884c67237e553cb0a60aa371d9c53b40e37d22
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 21:58:30 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
 
-class Pelicula(Base):
+class PeliculaDB(Base):
     __tablename__ = "peliculas"
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String(100), nullable=False)
     genero = Column(String(50))
-    duracion = Column(Integer)  # en minutos
+    duracion = Column(Integer)
     fecha_estreno = Column(DateTime)
