7aabe744dd292af959b3de262a68c30032e9dd25
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 22:14:23 -0500

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,16 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 
-class PeliculaCreate(BaseModel):
+class PeliculaBase(BaseModel):
     titulo: str = Field(..., example="Matrix")
     genero: str = Field(..., example="Acción")
     duracion: int = Field(..., example=120)
     fecha_estreno: datetime = Field(..., example="1999-03-31 00:00:00")
 
-class Pelicula(PeliculaCreate):
+class PeliculaCreate(PeliculaBase):
+    pass
+
+class Pelicula(PeliculaBase):
     id: int
 
     class Config:
