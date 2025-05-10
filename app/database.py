b6d5eeb4a0d1eff69e7cbc7bf9a949e61f3705c2
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 22:13:36 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MYSQL_HOST = os.getenv("MYSQL_HOST")  # IP privada MV BD
-MYSQL_PORT = os.getenv("MYSQL_PORT")  # 8008
-MYSQL_USER = os.getenv("MYSQL_USER")  # root
+# Configuración de MySQL (puerto 8008)
+MYSQL_HOST = os.getenv("MYSQL_HOST")       # IP privada de la MV de BD
+MYSQL_PORT = os.getenv("MYSQL_PORT")       # 8008
+MYSQL_USER = os.getenv("MYSQL_USER")       # root
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")  # utec
-MYSQL_DB = os.getenv("MYSQL_DB")  # bd_cartelera
+MYSQL_DB = os.getenv("MYSQL_DB")           # bd_cartelera
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
