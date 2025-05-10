dc45fbc9c1e822cd21767a6747cf691da804d31f
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 22:26:50 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Configuración de MySQL (puerto 8008)
-MYSQL_HOST = os.getenv("MYSQL_HOST")       # IP privada de la MV de BD
-MYSQL_PORT = os.getenv("MYSQL_PORT")       # 8008
-MYSQL_USER = os.getenv("MYSQL_USER")       # root
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")  # utec
-MYSQL_DB = os.getenv("MYSQL_DB")           # bd_cartelera
+# Configuración de MySQL
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
@@ -19,3 +19,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# ¡Añade esta función!
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
