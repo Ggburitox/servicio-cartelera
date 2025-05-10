da18f68978b07bce320eaa1a031b54ae4e54ac97
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-05-09 23:03:16 -0500

    Create pelicula.py

---
+++ app/schemas/pelicula.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class PeliculaBase(BaseModel):
+    titulo: str = Field(..., example="Matrix")
+    genero: str = Field(..., example="Acción")
+    duracion: int = Field(..., example=120)
+    fecha_estreno: datetime = Field(..., example="1999-03-31T00:00:00")
+
+class PeliculaCreate(PeliculaBase):
+    pass
+
+class Pelicula(PeliculaBase):
+    id: int
+    
+    class Config:
+        from_attributes = True
